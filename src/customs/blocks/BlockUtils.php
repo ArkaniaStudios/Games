852b7a7e44bf2387338393cd529c6b3689a795e4
Nassim E <nassim.essid91@gmail.com>
2024-06-17 19:09:18 +0200

    fix 😒

--- src/customs/blocks/BlockUtils.php
+++ src/customs/blocks/BlockUtils.php
@@ -38,6 +38,6 @@ class BlockUtils {
         $material = new Material(Material::TARGET_ALL, $texture, Material::RENDER_METHOD_ALPHA_TEST);
         $creative = new CreativeInventoryInfo($category, $group);
         $model = new Model([$material], $geometry, new Vector3(-8, 0, -8), new Vector3(16, 16, 16), $solid);
-        CustomiesBlockFactory::getInstance()->registerBlock(static fn () => new $class(), "olympia:" . $identifier, $model, $creative);
+        CustomiesBlockFactory::getInstance()->registerBlock(static fn () => new $class(), "arkania:" . $identifier, $model, $creative);
     }
 }
